663166d42b446fb8f71f0c2a8f288f0a62f33f4f
dr-copper <info@mannymejia.com>
2021-07-08 21:58:01 -0500

    main function update
    My apologies in the delay, the updated program has been tested and should be working with any rework: 
    
    - Main function was erroneously in the middle of the script (fixed) 
    - Index.html was not located in the templates folder (fixed) 
    - Created a function called create_upload_folder() which creates the upload folder on start if it's not there (fixed) 
    - Included app.secret_key as to sign cookies as a preventative measure against cookie tampering (added) 
    - Changed the app.config['UPLOAD_PATH'] = os.path.join(os.path.dirname(__file__), 'uploads') to make it more consistent throughout the app (fixed) 
    
    Please advise if for any reason there is still a persistent issue. Thank you

--- bundleup.py
+++ bundleup.py
@@ -9,16 +9,16 @@ from flask_cors import CORS, cross_origin
 #create low level functional client 
 client=boto3.client(
     's3',
-    aws_access_key_id = 'AKIATMGB4WODMOAH6JME',
-    aws_secret_access_key = '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',
+    aws_access_key_id = 'AKIATMGB4WODG3M6B3B5',
+    aws_secret_access_key = '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',
     region_name = 'us-east-1' 
     )
 
 #create high level object oriented interface 
 resource = boto3.resource(
     's3',
-    aws_access_key_id = 'AKIATMGB4WODMOAH6JME',
-    aws_secret_access_key = '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',
+    aws_access_key_id = 'AKIATMGB4WODG3M6B3B5',
+    aws_secret_access_key = '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',
     region_name = 'us-east-1' 
     )
 
@@ -33,7 +33,7 @@ for bucket in clientResponse['Buckets']:
 app = Flask(__name__, template_folder='.')
 app.config['MAX_CONTENT_LENGTH'] = 26000 * 1024 * 1024 #accept files up to 25GB in size
 app.config['UPLOAD EXTENSIONS'] = ['.tgz'] #rejects any non-matching file type
-app.config['UPLOAD_PATH'] = 'uploads'
+app.config['UPLOAD_PATH'] = os.path.join(os.path.dirname(__file__), 'uploads')
 
 CORS(app, support_credentials=True) #enable CORS for all routes
 
@@ -41,9 +41,6 @@ CORS(app, support_credentials=True) #enable CORS for all routes
 @cross_origin(support_credentials=True) #sending cookies along the payload back and forth
 def login():
     return jsonify({'success': 'ok'})
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
 
 @app.route('/') 
 def index(): #function will accept GET requests
@@ -54,22 +51,27 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in {'tgz'} #1st layer of filetype validation / procedure A check
 
+def upload_file(file):
+    file.save(os.path.join(app.config['UPLOAD_PATH'], file.filename)) #setup upload folder
+
 @app.route('/', methods=['POST'])
 def upload_files():
     if 'file' not in request.files:
         flash('No file part')
 
-    if filename != '' and filename.endswith('.tgz'): #2nd layer of filetype validation / procedure B check
-        file_ext = os.path.splitext(filename)[1]
-      
-    file = request.files['file'] #get name of uploaded file
+    file = request.files['user_file'] #get name of uploaded file
+
+    if file.filename != '' and file.filename.endswith('.docx'): #2nd layer of filetype validation / procedure B check
+        file_ext = os.path.splitext(file.filename)[1]
+    
     if file.filename == '':
         flash('No selected file')
         return redirect(request.url)
 
     if file and allowed_file(file.filename): # check if filetype is allowed
+        print('Hello')
         filename = secure_filename(file.filename) #make filename safe, remove unsupported chars / prevents possible exploit 
-        file.save(os.path.join(app.config['UPLOAD_PATH'], file.filename)) #setup upload folder
+        upload_file(file) # file.save(os.path.join(app.config['UPLOAD_PATH'], file.filename)) #setup upload folder
         return redirect(request.url)
     return '', 204 
 
@@ -78,6 +80,17 @@ def uploads():
     pass
     # return file.
 
+def create_upload_folder():
+    print(app.config['UPLOAD_PATH'])
+    if not os.path.exists(app.config['UPLOAD_PATH']):
+        os.makedirs(app.config['UPLOAD_PATH'])
+
 @app.errorhandler(413)
 def too_large(e):
     return "File is too large", 413
+
+if __name__ == "__main__":
+    create_upload_folder()
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(host='0.0.0.0', port=5000, debug=True)
